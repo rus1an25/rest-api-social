8c89277d5919cecd34f9075115009b0526ad4f10
Ruslan-Synytsia <mymessage02@gmail.com>
2022-03-17 13:00:01 +0200

    up-00

--- Components/Routers/authRouter.js
+++ Components/Routers/authRouter.js
@@ -7,11 +7,6 @@ const {body} = require("express-validator");
 
 router.get("/data", authController.getAuthData);
 
-// router.post("/register", [
-//     check('userName', 'Pole cant be is empty').notEmpty(),
-//     check('password', 'Password dont be less than 4 and more than 12 symbols').isLength({min: 4, max: 12})
-// ], authController.registrUser);
-
 router.post("/registration",
     body('userName').notEmpty(),
     body('email').isEmail(),
@@ -25,7 +20,5 @@ router.post("/logout", authController.logout);
 router.get("/activation/:url", authController.activation);
 
 router.get("/refresh", authController.refresh);
-//Only for tests
-router.get("/test/users", authMiddleware, authController.getTestUsers);
 
 module.exports = router;
